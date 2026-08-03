8ce23d7981fefec97fca4ab1e7d9abee2bba33cb
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-03 06:32:42 -0400

    Enable immersive mode across all activities

--- app/src/main/java/dev/alastorkaneki/adbovertcp/AdbTcpApplication.kt
+++ app/src/main/java/dev/alastorkaneki/adbovertcp/AdbTcpApplication.kt
@@ -1,11 +1,33 @@
 package dev.alastorkaneki.adbovertcp
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import com.google.android.material.color.DynamicColors
 
 class AdbTcpApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
+        registerActivityLifecycleCallbacks(
+            object : ActivityLifecycleCallbacks {
+                override fun onActivityCreated(activity: Activity, state: Bundle?) {
+                    ImmersiveMode.install(activity)
+                }
+
+                override fun onActivityResumed(activity: Activity) {
+                    ImmersiveMode.hide(activity)
+                }
+
+                override fun onActivityDestroyed(activity: Activity) {
+                    ImmersiveMode.uninstall(activity)
+                }
+
+                override fun onActivityStarted(activity: Activity) = Unit
+                override fun onActivityPaused(activity: Activity) = Unit
+                override fun onActivityStopped(activity: Activity) = Unit
+                override fun onActivitySaveInstanceState(activity: Activity, state: Bundle) = Unit
+            }
+        )
     }
 }
